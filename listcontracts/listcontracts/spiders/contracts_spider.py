0b4e49a1c368beb639abdf4644dd1e6f466dd533
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-01-19 17:20:11 +0200

    project initial assets

---
+++ listcontracts/listcontracts/spiders/contracts_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+class ContactSpider(scrapy.Spider):
+    name = "contracts"
+    start_urls = [
+        'https://apps.nd.gov/csd/spo/services/bidder/listCurrentContracts.htm',
+    ]
+
+    def parse(self, response):
+        print(f"responses now: {response}")
+        for contract in response.css('table tr'):
+            yield {
+                'contract_name': contract.css('td:nth-child(1) a::text').get(),
+                'contract_number': contract.css('td:nth-child(2)::text').get(),
+                'vendor_name': contract.css('td:nth-child(3)::text').get(),
+            }
---
+++ settings.py
@@ -0,0 +1,8 @@
+# settings.py
+
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
+
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
